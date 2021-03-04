0f8caf14e727228a1ce28f66b0b724ad03482b5f
swetha <swetha@email.com>
2021-03-05 00:41:13 +0530

    added test cases

---
+++ src/test/java/com/qa/testcases/TestCase1.java
@@ -0,0 +1,29 @@
+package com.qa.testcases;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+@Test
+public static void main(String[] args) throws InterruptedException //Test method
+{
+        
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		 WebDriver driver = new ChromeDriver();
+		 
+		 driver.manage().window().maximize();
+		 
+		 driver.get("http://18.191.161.86:8082/");
+		 
+		 Thread.sleep(2000);
+		 
+		 driver.findElement(By.linkText("About Us")).click();
+		
+		 }
+}
+
